54f1dda7c5649b568a2d1b6ed9f36b069d346d37
Nikita Vasilyev <me@elv1s.ru>
2010-09-09 18:09:49 +0400

    `Class` sounds better than `constructor`.

--- src/jquery.horizontal_box_slider.js
+++ src/jquery.horizontal_box_slider.js
@@ -5,18 +5,18 @@ jQuery.fn.horizontalBoxSlider = function(boxes) {
 	if (typeof boxes == "string") {
 		boxes = this.find(boxes);
 	}
-	return new jQuery.fn.horizontalBoxSlider.constructor(this[0], boxes);
+	return new jQuery.fn.horizontalBoxSlider.Class(this[0], boxes);
 };
 
-jQuery.fn.horizontalBoxSlider.constructor = function JQueryHorizontalBoxSlider(element, boxes) {
+jQuery.fn.horizontalBoxSlider.Class = function JQueryHorizontalBoxSlider(element, boxes) {
 	this.initialize(element, boxes);
 };
 
-jQuery.fn.horizontalBoxSlider.constructor.prototype = new HorizontalBoxSlider;
+jQuery.fn.horizontalBoxSlider.Class.prototype = new HorizontalBoxSlider;
 
 /**
  * @param {number} [x] >= 0
  */
-jQuery.fn.horizontalBoxSlider.constructor.prototype.left = function(x) {
+jQuery.fn.horizontalBoxSlider.Class.prototype.left = function(x) {
 	$(this.element).animate({"scrollLeft": x}, "fast");
 };
